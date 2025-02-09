b4d97db27a61d553ac3778fc24c6b23ebf01aad4
YusukeTakatsu <github ys.taka44@gmail.com>
2025-02-10 08:25:32 +0900

    feat:add workflow

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,17 +3,25 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+        $profile = Profile::factory()->for($user)->create();
 
-        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', ['id' => $user->id], 'template_mysql');
+        $this->assertDatabaseHas('profiles', ['id' => $profile->id]);
     }
 }
